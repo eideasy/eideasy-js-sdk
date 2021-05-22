0012d00b57a0dc0a219afa86f4c4fc456c8a7d77
matsjoonas <mj@codelight.eu>
2021-05-22 14:13:57 +0300

    Tweak the styles

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   title: 'eID Easy javascript SDK',
   description: 'eideasy-js-sdk provides you with a simple set of functions to get the user\'s identity.',
+  head: [['link', { rel: 'icon', href: '/assets/img/eid-easy-icon.png' }]],
   themeConfig: {
     repo: 'eideasy/eideasy-js-sdk/',
     logo: '/assets/img/eid-easy-logo.png',
