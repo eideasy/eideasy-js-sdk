f629268317c82244cce080fcaf10d849d6313042
matsjoonas <mj@codelight.eu>
2021-07-07 13:36:06 +0300

    feat(config): add the initial config.js

---
+++ src/config.js
@@ -0,0 +1,20 @@
+const clientMethods = {
+  authentication: [
+    {
+      action_type: 'ee-id-login',
+      handlingModule: 'idCard',
+      countries: ['EE'],
+    },
+  ],
+  signing: [],
+};
+
+// https://documenter.getpostman.com/view/3869493/Szf6WoG1#f920a591-838b-4f95-b105-0c8f55b41165
+// TODO: fetch the user config from eideasy and combine with clientMethods
+const availableMethods = {
+
+};
+
+const apiEndpoints = {
+
+};
