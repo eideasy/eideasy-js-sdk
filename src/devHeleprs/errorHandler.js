885cdcd8924820dceafe9e23e81116a09c23896d
matsjoonas <mj@codelight.eu>
2021-07-13 14:21:25 +0300

    feat: add userMessage and userDetails  to error

--- src/devHeleprs/errorHandler.js
+++ src/devHeleprs/errorHandler.js
@@ -1,7 +1,21 @@
 /* eslint-disable no-param-reassign */
 function createErrorHandler(errorContainer) {
   function show(error) {
-    errorContainer.innerHTML = error.toString();
+    let errorHtml = '';
+    if (error.userMessage) {
+      errorHtml += `<div>${error.userMessage}</div>`;
+    }
+
+    if (error.userDetails) {
+      Object.keys(error.userDetails).forEach((key) => {
+        errorHtml += `<div>${error.userDetails[key]}</div>`;
+      });
+    }
+
+    if (!errorHtml) {
+      errorHtml += `<div>${error.toString()}</div>`;
+    }
+    errorContainer.insertAdjacentHTML('beforeend', errorHtml);
     errorContainer.classList.remove('d-none');
   }
 
